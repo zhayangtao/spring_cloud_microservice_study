e7333752bf6aa3fb0264b429bcd4f7d416b797b8
zhayangtao <15313039096@163.com>
2018-01-23 15:28:21 +0800

    增加集群服务发现 microservice-discovery-eureka2

---
+++ microservice-discovery-eureka2/src/main/java/com/zyt/microservicediscoveryeureka2/MicroserviceDiscoveryEureka2Application.java
@@ -0,0 +1,14 @@
+package com.zyt.microservicediscoveryeureka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceDiscoveryEureka2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiscoveryEureka2Application.class, args);
+	}
+}
